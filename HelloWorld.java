4d5e68cb2d84144d49131a5186875ce4ff3a6c52
Sigotron <sigotron@gmail.com>
2014-01-19 15:25:54 -0800

    HelloWorld.java
    First Java File Uploaded.

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+/**
+ * First GitHub Java file commit!
+ * @author Sigotron
+ * @version 1/19/2014
+ */
+ public class HelloWorld {
+ 
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD! HELLO GITHUB!");
+	}
+ }
